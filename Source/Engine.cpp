6b98f15b89e8b5571a314815ee3ff7dc935870d5
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-12-01 23:07:24 +0100

    Save work in progress

--- Source/Engine.cpp
+++ Source/Engine.cpp
@@ -2,6 +2,9 @@
 #include <fstream>
 #include <Engine/Engine.h> 
 #include <iostream>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <string>
 
 void Engine::PrintHello()
 {
@@ -17,5 +20,21 @@ void Engine::PrintHello()
     {
         std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
         std::cout << "Could not open the engine config file.\n";
+    }
+
+    //Initialize SDL window and renderer
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+        std::cout << "Subsystems Initialized... " << std::endl;
+        auto Window = SDL_CreateWindow("SDL TEST", 0, 0, 400, 400, false);
+
+        if (Window) {
+            std::cout << "Window Created..." << std::endl;
+            auto Renderer = SDL_CreateRenderer(Window, NULL);
+
+            if (Renderer) {
+                std::cout << "Renderer Created" << std::endl;
+                SDL_SetRenderDrawColor(Renderer, 255, 255, 255, 255);
+            }
+        }
     }
 }
